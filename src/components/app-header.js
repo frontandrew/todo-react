043166f1ad32a5492f940b9325460e06ac7213be
Andrew Bubnov <jackblack.nt@ya.ru>
2020-04-23 17:26:17 +1100

    Code separated on modules

---
+++ src/components/app-header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AppHeader = () => <h1>ToDo List</h1>;
+
+export default AppHeader;
---
+++ src/components/todo-list-item.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TodoListItem = () => <span>Some to do item</span>
+
+export default TodoListItem;
