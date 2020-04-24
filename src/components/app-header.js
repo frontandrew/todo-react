1150fff4738ed8e4f82b4c61351cc53f5116c96b
Andrew Bubnov <jackblack.nt@ya.ru>
2020-04-25 04:32:17 +1100

    Added base layouts; Added modules structure; Added deployment scripts to gh-pages and install gh-pages plugin.

--- src/components/app-header.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const AppHeader = () => <h1>ToDo List</h1>;
-
-export default AppHeader;
--- src/components/todo-list-item.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const TodoListItem = () => <span>Some to do item</span>
-
-export default TodoListItem;
