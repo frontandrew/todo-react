22452b09d23ea2ad75d5fbb862d5d0e98df4a36c
Andrew Bubnov <jackblack.nt@ya.ru>
2020-09-08 22:56:08 +0500

    Prevent default on submit has been added.

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -12,12 +12,17 @@ import './item-add-form.css';
 
 export const ItemAddForm = () => {
   const { textAddItemInput, textNewItem } = useStore($lang);
-  const addInput = useStore($addInput);  
+  const addInput = useStore($addInput);
+
+  const onChange = e => {
+    e.preventDefault();
+    onAddItem();
+  }
     
     return (
       <form
         className="item-add-form d-flex"
-        onSubmit={onAddItem}>
+        onSubmit={e => onChange(e)}>
         <input
           type="text"
           placeholder={textAddItemInput}
