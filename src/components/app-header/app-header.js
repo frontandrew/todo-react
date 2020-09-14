fd6bf4af569f14f0d33a144ba83a60ff800eded5
Andrew Bubnov <jackblack.nt@ya.ru>
2020-09-15 01:25:35 +0500

    Lanf button has been changed.

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Button, makeStyles } from '@material-ui/core'
+
 import { useStore } from 'effector-react'
 import { $lang, $haveToDos, $haveDone, onToggleLang } from '../app/app.model'
 
 import './app-header.css'
+
+const styles = makeStyles({
+  langButtonRoot: {
+    fontWeight: '700',
+    fontSize: '0.675rem',
+    width: '40px',
+    minWidth: 0,
+    position: "absolute",
+    opacity: .8,
+    top: '-14px',
+    right: 0,
+    '&:hover': {
+      opacity: 1,
+    }
+  },
+})
 
 export const AppHeader = () => {
   const {
@@ -15,14 +33,17 @@ export const AppHeader = () => {
   const toDo = useStore($haveToDos)
   const done = useStore($haveDone)
 
+  const { langButtonRoot, lable } = styles();
+
   return (
     <div className="app-header d-flex ">
       <h1>{textToDoList}</h1>
-      <button
-        className="btn btn-outline-secondary btn-sm app-header__lang"
+      <Button
+        classes={{ root: langButtonRoot }}
+        variant='outlined'
         onClick={() => onToggleLang(id)}>
         {label}
-      </button>
+      </Button>
       <h2>{toDo}{textMoreToDo}{done}{textDone}</h2>
     </div>
   )
